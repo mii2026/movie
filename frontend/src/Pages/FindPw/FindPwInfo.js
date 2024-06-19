601a7e3afe68befa845b5ee67ac1e9298aa448d0
mii2026 <mii2026@skku.edu>
2024-06-19 23:51:55 +0900

    [feat] 비밀번호 찾기 페이지 이벤트 함수 생성

--- frontend/src/Pages/FindPw/FindPwInfo.js
+++ frontend/src/Pages/FindPw/FindPwInfo.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import '../../Style/Global.css'
 import '../../Style/FindPw.css';
 
 const FindPwInfo= () => {
+  // 사용자 정보
+  const [username, setName] = useState("");
+  const[birthday, setBirthday] = useState("");
+  const[email, setEmail] = useState("");
   
   const navigate = useNavigate();
+
+  const setUserName = event => {
+    const value = event.target.value;
+    const not = /[~!@#$%";'^,&*()_+|</>=>`?:{[}]/g;
+    
+    setName(value.replace(not, ""));
+  }
+
+  const setUserBirthday = event => {
+    const value = event.target.value;
+    const not = /[^0-9]/g;
+
+    setBirthday(value.replace(not, ""));
+  }
+
+  const setUserEmail = event => {
+    setEmail(event.target.value);
+  }
 
   const handleClick = () => {
     navigate('/findpw/send-pw')
@@ -21,15 +43,31 @@ const FindPwInfo= () => {
         <div className='findpw'>
           <div style={{marginBottom:"15px"}}>이름</div>
           <div>
-            <input type="text" placeholder="이름을 입력하세요"/>
+            <input 
+              type="text" 
+              placeholder="이름을 입력하세요"
+              value={username}
+              onChange={setUserName}
+            />
           </div>
           <p>생년월일</p>
           <div>
-            <input type="text" placeholder="생년월일을 입력하세요 (ex: 19000101)" />
+            <input 
+              type="text" 
+              placeholder="생년월일을 입력하세요 (ex: 19000101)" 
+              value={birthday}
+              onChange={setUserBirthday}
+              maxLength={8}  
+            />
           </div>
           <p>이메일</p>
           <div>
-            <input type="email" placeholder="이메일을 입력하세요"/>
+            <input 
+              type="email" 
+              placeholder="이메일을 입력하세요"
+              value={email}
+              onChange={setUserEmail}  
+            />
           </div>
           <div className='enter50'></div>
         </div>
